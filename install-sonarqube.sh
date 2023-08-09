b6e123c5f625a47a7d09364f1f14599463b0956a
Ian Ellis <ellisiana@gmail.com>
2023-08-09 01:25:51 +0000

    Sirt out pv for kubectl/kind

--- install-sonarqube.sh
+++ install-sonarqube.sh
@@ -1,17 +1,35 @@
-# Make sure kind cluster exists
-kind  get clusters 2>&1 | grep "kind-sonarqube"
-if [ $? -gt 0 ]
-then
-    envsubst < kind-config.yml.template > kind-config.yml
-    kind create cluster --config kind-config.yml --name kind-sonarqube
+unset USE_KIND
+# Check if kubectl is available in the system
+if kubectl 2>/dev/null >/dev/null; then
+  # Check if kubectl can communicate with a Kubernetes cluster
+  if kubectl get nodes 2>/dev/null >/dev/null; then
+    echo "Kubernetes cluster is available. Using existing cluster."
+    export USE_KIND=0
+  else
+    echo "Kubernetes cluster is not available. Creating a Kind cluster..."
+    export USE_KIND=X
+  fi
+else
+  echo "kubectl is not installed. Please install kubectl to interact with Kubernetes."
+  export USE_KIND=X
 fi
 
-# Make sure create cluster succeeded
-kind  get clusters 2>&1 | grep "kind-sonarqube"
-if [ $? -gt 0 ]
-then
-    echo "Creation of cluster failed. Aborting."
-    exit 666
+if [ "X${USE_KIND}" == "XX" ]; then
+    # Make sure kind cluster exists
+    kind  get clusters 2>&1 | grep "kind-sonarqube"
+    if [ $? -gt 0 ]
+    then
+        envsubst < kind-config.yml.template > kind-config.yml
+        kind create cluster --config kind-config.yml --name kind-sonarqube
+    fi
+    
+    # Make sure create cluster succeeded
+    kind  get clusters 2>&1 | grep "kind-sonarqube"
+    if [ $? -gt 0 ]
+    then
+        echo "Creation of cluster failed. Aborting."
+        exit 666
+    fi
 fi
 
 # add metrics
@@ -28,8 +46,14 @@ then
 fi
 
 # sort out persistent volume
-export NODE_NAME=$(kubectl get nodes |grep control-plan|cut -d\  -f1)
-envsubst < sonarqube.deploy.pv.yml.template > sonarqube.deploy.pv.yml
+if [ "X{$USE_KIND}" == "XX" ];then
+  export NODE_NAME=$(kubectl get nodes |grep control-plane|cut -d\  -f1|head -1)
+  envsubst < sonarqube.deploy.pv.kind.yml.template > sonarqube.deploy.pv.yml
+else
+  export NODE_NAME=$(kubectl get nodes | grep -v ^NAME|grep -v control-plane|cut -d\  -f1|head -1)
+  envsubst < sonarqube.deploy.pv.linux.yml.template > sonarqube.deploy.pv.yml
+  echo mkdir -p ${PWD}/nexus-data|ssh -o StrictHostKeyChecking=no ${NODE_NAME}
+fi
 kubectl apply -f sonarqube.deploy.pv.yml
 
 # create common deployment
